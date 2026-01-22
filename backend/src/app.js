bc693dd8977a950620abc51c953c140bb19afd25
Aditya Choudhary <chyaditya775@gmail.com>
2026-01-22 17:28:42 +0530

    Add certain origins to cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,26 @@ const errorMiddleware = require("./middlewares/error.middleware");
 
 const app = express();
 
-app.use(cors());
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://todo-drab-rho.vercel.app/",
+];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    }
+
+    return callback(new Error("Not allowed by CORS"));
+  },
+  methods: ["GET", "POST", "PATCH", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+}));
+
 app.use(express.json());
 
 app.use("/todos", todoRoutes);
